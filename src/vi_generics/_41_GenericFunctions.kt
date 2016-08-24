5cf987dd768ba0cf12409018c48e0ff3ec63206f
ts25504 <ts25504@gmail.com>
2016-08-24 14:29:52 +0800

    finish vi_generics

--- src/vi_generics/_41_GenericFunctions.kt
+++ src/vi_generics/_41_GenericFunctions.kt
@@ -19,12 +19,21 @@ fun task41(): Nothing = TODO(
         }
 )
 
+fun <T, C: MutableCollection<T>> Collection<T>.partitionTo(a: C, b: C, init: (T) -> Boolean): Pair<C, C> {
+    for (e in this) {
+        if (init(e)) {
+            a.add(e)
+        } else {
+            b.add(e)
+        }
+    }
+    return Pair(a, b)
+}
+
 fun List<String>.partitionWordsAndLines(): Pair<List<String>, List<String>> {
-    task41()
-//    return partitionTo(ArrayList<String>(), ArrayList()) { s -> !s.contains(" ") }
+    return partitionTo(ArrayList<String>(), ArrayList()) { s -> !s.contains(" ") }
 }
 
 fun Set<Char>.partitionLettersAndOtherSymbols(): Pair<Set<Char>, Set<Char>> {
-    task41()
-//    return partitionTo(HashSet<Char>(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z'}
+    return partitionTo(HashSet<Char>(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z'}
 }
